f9dfacfa20211d4066518ba1b2ebe3b21216b137
Nola Stowe <nola@gamesalad.com>
2011-08-12 09:41:05 -0500

    initial build of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubypair::Application.config.secret_token = '3cf7b9328f8e9b29937b49ea3b361527ce4c09d6b86c0b7d6bcfedeebc800d0b1b6fdab76db393319ec5ff5358bece4329d9945d4e0a74544dc6073c70c2fde9'
