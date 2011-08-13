47fae1fc2946ef9e6f03451503780fc069206dfc
Evan Light <evan.light@tripledogdare.net>
2011-08-13 13:15:18 -0400

    Sign out and sign in now behaving itself

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
-  
+
   private
-  
+
   def current_user
-    @current_user ||= User.find(:github_login => session[:github_login]) if session[:github_login]
+    if session[:github_login]
+      @current_user ||= User.where(:github_login => session[:github_login]).first
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rubypair::Application.routes.draw do
-  match '/auth/:provider/callback' => "sessions#create"
+  match '/auth/:provider/callback' => 'sessions#create'
+  match '/signout' => 'sessions#destroy'
 
   root :to => "home#index"
 end
