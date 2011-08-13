39b5e75b868f3e87a8f127f09a61ff16ca365988
Evan Light <evan.light@tripledogdare.net>
2011-08-13 12:23:14 -0400

    Replaces ActiveRecord with Mongoid

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
 
 # If you have a Gemfile, require the default gems, the ones in the
 # current environment and also include :assets gems if in development
